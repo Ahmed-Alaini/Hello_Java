f006ddb0e9c81cf8c2a35e7f65579b0ed32f7011
77Ahmed <alainiahmed89@gmail.com>
2024-06-29 06:30:27 +0300

    start learning java

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.helloworld;
+
+import static android.content.Intent.makeMainActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Oncreate() is called. ", Toast.LENGTH_SHORT).show();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this, "onStart() is called. ", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "onResume() is called. ", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "onPause() is called. ", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop() is called. ", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy() is called. ", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart() is called. ", Toast.LENGTH_SHORT).show();
+
+    }
+    public void buttonIsClickd(){}
+
+}
